3771f804f96c67fa678e96187f9e1d19499b20d7
JAlejandroSG10 <alejito0968@gmail.com>
2024-05-23 17:26:45 -0500

    Error fixed

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -3,12 +3,9 @@ const bcrypt = require('bcrypt')
 const user = require('../Models/user')
 const jwt = require('jsonwebtoken')
 
-
-//const jwtPassword = 'qwe'
-
-
-
-
+//ESTO ES SOLO PARA LA PRÁCTICA DEL EJERCICIO DEL JWT - ESTO NO ES RECOMENDADO PARA NINGUN DESARROLLO
+//THIS IS ONLY FOR JWT EXERCISE PRACTICE - THIS IS NOT RECOMMENDED FOR ANY DEVELOPMENT
+const jwtPassword = 'qwe987gfd'
 
 async function listUserRoles(req, res){
     try{
@@ -23,15 +20,13 @@ async function createUser(req, res){
     try{
         const hashPassword = await bcrypt.hash(req.body.userPassword, 10)
 
-        console.log(req.body);
-
         await user.create({
             userIdentification: req.body.userIdentification,
-            userIdentificationType: req.body.userIdentificationType,
-            userName: req.body.userName,
+            userIdentificationType : req.body.userIdentificationType,
+            userName : req.body.userName,
             userPassword: hashPassword,
-            userRole: req.body.userRole
-        }).then(function(data){
+            userRole: req.body. userRole
+        }).then(function (data){
             return res.status(200).json({
                 data: data
             });
@@ -41,14 +36,14 @@ async function createUser(req, res){
             });
         })
     }
-    catch(e){
+    catch (e){
         console.log(e);
     }
 }
 
 async function login (req, res){
     try{
-        const userData = await user.findAll({where: {userIdentification: req.body.userIdentification}})
+        const userData = await user.findOne({ where: { userIdentification: req.body.userIdentification}})
 
         if(!userData)
             return res.status(401).json({message: "User not found"})
@@ -59,17 +54,16 @@ async function login (req, res){
             return res.status(401).json({message: "Invalid password"})
 
         const token = jwt.sign(
-            {userId: userData.userId, userRole: userData.userRole},
-            //jwtPassword,
-            {expiresIn}
+            { userId: userData.userId, userRole: userData.userRole },
+            jwtPassword,
+            { expiresIn: '1h'}
         )
 
-        return res.status(200).json({message: "Welcome"})
+        return res.status(200).json({ token })
     }
-    catch(e){
-        console.log(e);
+    catch (e){
+        console.log(e)
     }
-    
 }
 
 module.exports ={
